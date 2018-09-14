ef4151b088330f5afc08d1d1d503502422de6dc9
peace_data <kishetmmm@gmail.com>
2018-09-14 19:58:01 +0300

    enrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export RAILS_ENV=production
+
 set -e
 run_cmd="bundle exec rake assets:clean \
     && bundle exec rake assets:precompile \
@@ -8,13 +10,15 @@ run_cmd="bundle exec rake assets:clean \
     && bundle exec rake db:migrate \
     && bundle exec rake key_codes_definitions:parse:xml_codes \
     && bundle exec rake partners:import \
-    && bundle exec rake product_code:import \
-    && bundle exec puma -C config/puma.rb"
+    && bundle exec rake product_code:import" 
+#    && bundle exec puma -C config/puma.rb"
 
-until bundle ef database update; do
+until bundle exec rake; do
 >&2 echo "MariaDB Server is starting up"
 sleep 1
 done
 
 >&2 echo "MariaDB Server is up - executing command"
 exec $run_cmd
+
+bundle exec puma -C config/puma.rb
