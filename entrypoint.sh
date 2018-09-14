5dc70e47f0d722db8f721f69e89ecf93b13489cc
peace_data <kishetmmm@gmail.com>
2018-09-14 19:09:51 +0300

    script fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ run_cmd="bundle exec rake assets:clean \
     && bundle exec rake product_code:import \
     && bundle exec puma -C config/puma.rb"
 
-until dotnet ef database update; do
+until bundle ef database update; do
 >&2 echo "MariaDB Server is starting up"
 sleep 1
 done
