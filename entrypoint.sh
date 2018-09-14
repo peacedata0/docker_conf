e45a7c54fb18731fa9a9e758fd89463e9028cd67
peace_data <kishetmmm@gmail.com>
2018-09-14 20:21:10 +0300

    mini fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 export RAILS_ENV=production
 
-bundle exec rake assets:clean
-bundle exec rake assets:precompile
-bundle exec rake db:drop
-bundle exec rake db:create
-bundle exec rake db:migrate
-bundle exec rake key_codes_definitions:parse:xml_codes
-bundle exec rake partners:import
-bundle exec rake product_code:import
-bundle exec puma -C config/puma.rb
+rake assets:clean
+rake assets:precompile
+rake db:drop
+rake db:create
+rake db:migrate
+rake key_codes_definitions:parse:xml_codes
+rake partners:import
+rake product_code:import
+puma -C config/puma.rb
