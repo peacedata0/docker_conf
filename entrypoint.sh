2ecb6fd49651b737e0662918c30a0e40aea3e871
peace_data <kishetmmm@gmail.com>
2018-09-14 20:20:33 +0300

    script fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,12 @@
 
 export RAILS_ENV=production
 
-set -e
-run_cmd="bundle exec rake assets:clean \
-    && bundle exec rake assets:precompile \
-    && bundle exec rake db:drop \
-    && bundle exec rake db:create \
-    && bundle exec rake db:migrate \
-    && bundle exec rake key_codes_definitions:parse:xml_codes \
-    && bundle exec rake partners:import \
-    && bundle exec rake product_code:import" 
-#    && bundle exec puma -C config/puma.rb"
-
-until bundle exec rake; do
->&2 echo "MariaDB Server is starting up"
-sleep 1
-done
-
->&2 echo "MariaDB Server is up - executing command"
-exec $run_cmd
-
+bundle exec rake assets:clean
+bundle exec rake assets:precompile
+bundle exec rake db:drop
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake key_codes_definitions:parse:xml_codes
+bundle exec rake partners:import
+bundle exec rake product_code:import
 bundle exec puma -C config/puma.rb
